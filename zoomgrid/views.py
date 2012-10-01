e37111767385832eefd4335e5e4e394d265ff1aa
Bill Garr <wjg8@georgetown.edu>
2012-10-01 15:26:28 -0400

    pulling in json data for rows and columns.

--- zoomgrid/views.py
+++ zoomgrid/views.py
@@ -5,6 +5,7 @@ from django.template import RequestContext
 from django.core import serializers
 from django.utils import simplejson
 from django.core.urlresolvers import reverse
+from django.forms.models import model_to_dict
 
 from django.contrib.auth.models import User
 from zoomgrid.models import Grid
@@ -17,10 +18,13 @@ def home(request):
 	
 	# serialize grid & all its associated data & dump it as json onto the page.
 	json_serializer = serializers.get_serializer("json")()
-	
+	columns_json = json_serializer.serialize(grid_columns)
+	rows_json = json_serializer.serialize(grid_rows)
 	
 	return render_to_response('home.html',
 					{ 'grid':grid,
 					  'grid_columns':grid_columns,
-					  'grid_rows':grid_rows },
+					  'grid_rows':grid_rows,
+					  'columns_json':columns_json,
+					  'rows_json':rows_json	},
 					context_instance=RequestContext(request))
